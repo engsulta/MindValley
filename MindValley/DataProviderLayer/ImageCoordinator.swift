c7a5dde4249a54b542b32f34859ac8f9968398d6
Ahmed sultan <en.ahmedsultan@gmail.com>
2020-03-13 16:34:10 +0200

    image coordinator

---
+++ MindValley/DataProviderLayer/ImageCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCoordinator.swift
+//  MindValley
+//
+//  Created by Ahmed Sultan on 3/13/20.
+//  Copyright © 2020 hamza. All rights reserved.
+//
+
+import Foundation
+import Kingfisher
+
+final class ImageCoordinator {
+    typealias KingFisherCompletionHandler = (UIImage?, Error?) -> Void
+    static func getImage(withURLString urlString: String?, completion: @escaping KingFisherCompletionHandler) {
+        guard let photoURLString = urlString,
+            let url = URL(string: photoURLString) else {
+                completion(nil, nil)
+                return
+        }
+        
+        let imageCacheKey = photoURLString
+        let resource = ImageResource(downloadURL: url, cacheKey: imageCacheKey)
+        
+        KingfisherManager.shared.retrieveImage(with: resource, options: nil, progressBlock: nil) { (image, error, cacheType, url) in
+            DispatchQueue.main.async {
+                completion(image, error)
+            }
+        }
+    }
+}
